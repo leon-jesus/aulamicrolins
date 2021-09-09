603719ce515923523df335d78bf74cfe6cca36e9
leon-jesus <90413907+leon-jesus@users.noreply.github.com>
2021-09-09 14:59:45 -0300

    Add files via upload

---
+++ aula02-3.php
@@ -0,0 +1,19 @@
+<html lang="pt-BR">
+	<head>
+		<meta charset="UTF-8">
+		<title>Formatação de Texto e parágrafo</title>
+	</head>	
+	<body bgcolor="#000000" text="#ffffff" link="#ff0000" vlink=#ff00ff">
+		<font face="comic sans ms" size=5 color="#ffff00">
+		<p align="left">
+		O <b>Brasil</b> será representado por apenas quatro pessoas na cerimônia de abertura dos <i>Jogos Olímpicos de Tóquio</i>, <u>amanhã (23), a partir das 8 horas.</u><br> A cúpula do <b><i><u><s>Comitê Olímpico do Brasil</b></i></u></s> havia decidido, pela manhã do <sup>Japão</sub>, que apenas os <sub>porta-bandeiras</sub> e o chefe da missão estariam no evento. Mas foi constatada a necessidade de alguém que fizesse a interlocução entre os brasileiros e os organizadores. Assim, um oficial administrativo foi integrado à diminuta delegação....
+		<br>
+		X<sup>2</sup>+Y<sup>2</sup>=0
+		</p>
+		
+		<a href="aula1.php">Arquivo Aula 1</a><br>
+		<a href="/Ter-quin/aula2-1.php">Arquivo Aula 2-1</a><br>
+		<a href="http://www.microlins.com.br">Site da Microlins</a>
+		</font>
+	</body>
+</html>
