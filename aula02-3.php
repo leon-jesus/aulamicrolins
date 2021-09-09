b54f25dccec20165a0fae5d5a98e622bafa746b8
unknown <d.karim>
2021-09-09 15:23:51 -0300

    add texto

--- aula02-3.php
+++ aula02-3.php
@@ -7,7 +7,9 @@
 		<font face="comic sans ms" size=5 color="#ffff00">
 		<p align="left">
 		Registros históricos apontam que o primeiro carro surgiu em meados de 1678 e foi desenvolvido por um padre belga chamado Ferdinand Verbiest.<p> O padre teria feito o primeiro veículo terrestre, movido a vapor, para o imperador da China.<br> Mas, como o modelo era “primário” e movido a vapor, o padre não foi considerado, exatamente, o inventor do carro.<p>
-		<br>
+		O primeiro automóvel a rodar no Brasil foi trazido por Henrique Santos Dumont, em 1893. <br> Henrique era irmão de Alberto Santos Dumont, o famoso aeronauta, esportista e inventor, considerado o pai da aviação brasileira. <br>
+		O automóvel, que chegou ao Brasil de maneira inédita, era um Peugeot com motor Daimler movido a vapor. <p>
+		
 		X<sup>2</sup>+Y<sup>2</sup>=0
 		</p>
 		
